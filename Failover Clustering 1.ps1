f9c241325fff33d555455fe2b5df31922614e9e6
conradg69 <36333522+conradg69@users.noreply.github.com>
2018-10-09 15:03:41 +0100

    Updated for the less than 10 VM's

--- Failover Clustering 1.ps1
+++ Failover Clustering 1.ps1
@@ -8,10 +8,10 @@ Set-LabInstallationCredential -Username Install -Password Somepass1
 Add-LabVirtualNetworkDefinition -Name $labname -AddressSpace 192.168.50.0/24
 
 $PSDefaultParameterValues = @{
-    'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2016 SERVERDATACENTER'
+    'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2016 Datacenter (Desktop Experience)'
     'Add-LabMachineDefinition:Network'         = $labname
     'Add-LabMachineDefinition:DomainName'      = 'contoso.com'
-    'Add-LabMachineDefinition:Memory'          = 1GB
+    'Add-LabMachineDefinition:Memory'          = 2GB
 }
 
 Add-LabMachineDefinition -Name foDC1 -Roles RootDC
@@ -24,25 +24,15 @@ Add-LabMachineDefinition -Name foCLS1 -Roles $storageRole -DiskName LunDisk
 # Integrate one or more clusters
 # This sample will create two named clusters and one automatic cluster called ALCluster with an automatic static IP
 $cluster1 = Get-LabMachineRoleDefinition -Role FailoverNode -Properties @{ ClusterName = 'Clu1'; ClusterIp = '192.168.50.111' }
-$cluster2 = Get-LabMachineRoleDefinition -Role FailoverNode -Properties @{ ClusterName = 'Clu2'; ClusterIp = '192.168.50.121' }
-$cluster3 = Get-LabMachineRoleDefinition -Role FailoverNode
-foreach ( $i in 1..10 )
-{
-    if (-not ($i % 2))
-    {
-        Add-LabMachineDefinition -name foCLN$i -Roles $cluster1
-    }
-    elseif (-not ($i % 3))
-    {
-        Add-LabMachineDefinition -name foCLN$i -Roles $cluster2
-    }
-    else
-    {
-        Add-LabMachineDefinition -name foCLN$i -Roles $cluster3
-    }
+#$cluster2 = Get-LabMachineRoleDefinition -Role FailoverNode -Properties @{ ClusterName = 'Clu2'; ClusterIp = '192.168.50.121' }
+#$cluster3 = Get-LabMachineRoleDefinition -Role FailoverNode
+
+        Add-LabMachineDefinition -name foCLN1 -Roles $cluster1
+        Add-LabMachineDefinition -name foCLN2 -Roles $cluster1
     
-}
 
 Install-Lab
 
 Show-LabDeploymentSummary
+
+get-command AutomatedLab
